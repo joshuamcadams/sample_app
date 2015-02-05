59c0cc39545db85b132f937a609c003277632f4e
Josh McAdams <joshmcadams@gmail.com>
2015-02-05 10:02:31 -0800

    full title method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,13 +14,19 @@ describe "Static pages" do
 
 		end
 
-		it "should have the right title" do
+		it "should have the base title" do
 
 		visit '/static_pages/home'
-		page.should have_selector('title', :text => "#{pretitle} | Home")
+		page.should have_selector('title', :text => "#{pretitle}")
 
 		end
 
+		it "should not have a cusotm page title" do
+
+		visit '/static_pages/home'
+		page.should_not have_selector('title', :text => "| Home")
+
+		end
 		
 
 	end
