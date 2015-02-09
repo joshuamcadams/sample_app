34a9d917951c3fa00c8366bd5d90f1cd5a777888
Josh McAdams <joshmcadams@gmail.com>
2015-02-08 19:26:30 -0800

    sign in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /[\w+\-.]+@[\w+\-.]+\.[a-z]+/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_senitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
 end
