4bf3a576ac6a62e33a4b3f0e3c21715d08bec913
Josh McAdams <joshmcadams@gmail.com>
2015-02-09 17:13:05 -0800

    Add Cucumber Features

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -94,6 +95,14 @@ describe User do
 
 			it { should_not == user_for_invalid_password }
 			specify { user_for_invalid_password.should be_false }
+		end
+	end
+
+	describe "remember token" do 
+		before { @user.save }
+		its(:remember_token) { should_not be_blank}
+		it "should have nonblank remember token" do 
+			subject.remember_token.should_not be_blank
 		end
 	end
 
