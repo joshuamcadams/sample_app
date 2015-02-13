7100e918867ff24d041585f0865e1b1826ac61c1
Josh McAdams <joshmcadams@gmail.com>
2015-02-12 19:55:06 -0800

    add follwer posts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -6,6 +6,7 @@ describe "Micropost pages" do
 
 	describe "micropost creation" do
 		before { visit root_path }
+		
 		describe "with invalid information" do
 			it "should not create a micropost" do
 				expect { click_button "Post" }.to_not change(Micropost, :count)
@@ -19,6 +20,16 @@ describe "Micropost pages" do
 			before { fill_in 'micropost_content', with: "Lorem ipsum" }
 			it "should create a micropost" do
 				expect { click_button "Post" }.to change(Micropost, :count).by(1)
+			end
+		end
+	end
+
+	describe "micropost destruction" do
+		before { FactoryGirl.create(:micropost, user: user) }
+		describe "as correct user" do
+			before { visit root_path }
+			it "should delete a micropost" do
+				expect { click_link "delete" }.should change(Micropost, :count).by(-1)
 			end
 		end
 	end
